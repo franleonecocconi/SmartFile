1d3433c9e7f69fc46b5a864a385546b13b6b64dd
Francesco Leone Cocconi <escuelitadetecnologiatechyt367@gmail.com>
2026-05-01 19:15:07 -0300

    Update SmartFile

--- server.js
+++ server.js
@@ -35,6 +35,8 @@ io.on("connection", (socket) => {
 
 });
 
-server.listen(3000, "0.0.0.0", () => {
-    console.log("SmartFile iniciado en puerto 3000");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log("Servidor iniciado");
 });
